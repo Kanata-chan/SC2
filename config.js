8f8588dfb2122b41935f57f95249567110fc4f4a
Kanata-chan <septaraditya62@gmail.com>
2022-08-29 19:20:41 +0700

    SC2

---
+++ config.js
@@ -0,0 +1,60 @@
+/**
+* Thanks To
+* Allah S.W.T
+* Ortu
+* MR FIN (DEFF)
+* Dika Ardnt
+* Sanzy Kawaii
+* FIRAA
+* Guntur
+* Dhika Ardiant
+* All Creator Bot
+* All Subscriber Ku
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'Your Key',
+}
+
+// Other
+global.owner = ['628811524907']
+global.premium = ['628811524907']
+global.packname = 'HiragiBot MD'
+global.author = 'Hirachan'
+global.sessionName = 'Hirachan'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    eror: 'Yah Eror kak',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/apri.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
